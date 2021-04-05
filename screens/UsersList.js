928bff195619d17caa1573e040df8d5622fc3ff4
AaronDiazSkyion <chinobass3819@gmail.com>
2021-04-05 13:39:42 -0500

    subir

--- screens/UsersList.js
+++ screens/UsersList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { View, Switch, StyleSheet, Image, Text, Alert } from 'react-native';
-//Que onda mi LEO hahdad
+//Que onda mi LEO holaa
 
 
 export default class Foco extends Component {
