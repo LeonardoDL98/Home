077c293a47f25d1a80dfedfed3f89db1a5972a86
JoseLuisCervantesSalazar <jlcervantes240@gmail.com>
2021-04-06 23:44:19 -0500

    subi

--- screens/UsersList.js
+++ screens/UsersList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Switch, StyleSheet, Image, Text, Alert } from 'react-native';
 //hola
 //jejeje
+//maluma
 
 export default class Room1 extends Component {
   constructor(props) {
