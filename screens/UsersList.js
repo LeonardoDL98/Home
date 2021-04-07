3a0e79886cc4c6cb2c2c72acf527554a1cfa1bfb
Leonardo Adrian De Lira Castañon <leo_aguila9876@hotmail.com>
2021-04-06 23:44:57 -0500

    borrado comentario

--- screens/UsersList.js
+++ screens/UsersList.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { View, Switch, StyleSheet, Image, Text, Alert } from 'react-native';
-//hola
-//jejeje
-//maluma
+
 
 export default class Room1 extends Component {
   constructor(props) {
